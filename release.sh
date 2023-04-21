771ad828d962ab18fcc6f5f67094efa334a025f9
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-04-22 00:49:08 +0300

    Update to lunar

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ dpkg-sig --sign builder ./output/pika-settings*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter kinetic 'Package (% pika-settings*)'
+reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-settings*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb kinetic ./output/pika-settings*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-settings*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
